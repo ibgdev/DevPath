5153c9446256eb7ee419c70b7b92874e3fd1a826
Mayssabenchikh <abderrahmenmayssa@gmail.com>
2025-04-06 14:53:53 +0100

    not-found&home-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { CoursesComponent } from './users/courses/courses.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  {path: 'courses', component: CoursesComponent},
+  { path: '', component:HomeComponent},
+  { path: 'courses', component: CoursesComponent },
+  { path: '**', component: NotFoundComponent }
 ];
+
