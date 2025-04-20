db01a9ff51665e55881e4e5c4963e0ae0b80a9da
ibgdev <ibrahimghorbali605@gmail.com>
2025-04-20 11:49:15 +0100

    added routes + scss enhanced

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { CoursesComponent } from './users/courses/courses.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HomeComponent } from './home/home.component';
 import { DetailCourseComponent } from './users/detail-course/detail-course.component';
+import { AboutusComponent } from './users/aboutus/aboutus.component';
+import { MyaccountComponent } from './users/myaccount/myaccount.component';
+import { RoadmapgeneralComponent } from './users/roadmapgeneral/roadmapgeneral.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'courses', component: CoursesComponent },
+  { path: 'about-us', component: AboutusComponent },
+  { path: 'my-account', component: MyaccountComponent },
+  { path: 'roadmaps', component: RoadmapgeneralComponent },
   { path: 'course-details/:id', component: DetailCourseComponent },
   { path: '**', component: NotFoundComponent }
 ];
